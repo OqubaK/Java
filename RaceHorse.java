287cc66c0413a44e0d24b5589080db271164fcf4
Oquba Khan <136655471+OqubaK@users.noreply.github.com>
2023-06-19 18:11:53 -0400

    Add files via upload

---
+++ RaceHorse.java
@@ -0,0 +1,10 @@
+public class RaceHorse extends Horse{
+    int races;
+
+    public void setRaces(int races){
+        this.races = races;
+    }
+    public int getRaces(){
+        return races;
+    }
+}
